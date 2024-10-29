15016fc0f7bb497a66f99bbe1d14f4d23bcec92b
hiphop5782 <zdlsz@hanmail.net>
2024-10-29 11:56:25 +0900

    initial commit

---
+++ cocoatalk-be/src/main/java/com/hacademy/cocoatalk/CocoatalkBeApplication.java
@@ -0,0 +1,13 @@
+package com.hacademy.cocoatalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocoatalkBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocoatalkBeApplication.class, args);
+	}
+
+}
---
+++ cocoatalk-be/src/test/java/com/hacademy/cocoatalk/CocoatalkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacademy.cocoatalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocoatalkBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
